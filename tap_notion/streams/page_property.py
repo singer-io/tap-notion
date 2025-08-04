6a7c7c9ec8bdf5173d21f75d2eae47cbf8c89123
Rahul Gogawale <rahul.gogawale@globallogic.com>
2025-08-04 11:58:44 +0530

    sync running...

--- tap_notion/streams/page_property.py
+++ tap_notion/streams/page_property.py
@@ -1,13 +1,16 @@
 from typing import Dict, Any
 from singer import get_bookmark, get_logger
-from tap_notion.streams.abstracts import IncrementalStream
+from tap_notion.streams.abstracts import FullTableStream
 
 LOGGER = get_logger()
 
 
-class PagesProperty(IncrementalStream):
+class PagesProperty(FullTableStream):
     tap_stream_id = "pages_property"
     key_properties = ["id"]
     replication_method = "FULL_TABLE"
-    replication_keys = []
     path = "/v1/pages/{page_id}/properties/{property_id}"
+
+    @property
+    def replication_keys(self):
+        return []
