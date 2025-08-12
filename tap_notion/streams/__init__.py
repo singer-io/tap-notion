66f57e81bd11b32f54b6096322cf5606ee75efdd
Rahul Gogawale <rahul.gogawale@globallogic.com>
2025-08-12 15:06:21 +0530

    As per the comment changes made.

--- tap_notion/streams/__init__.py
+++ tap_notion/streams/__init__.py
@@ -6,10 +6,9 @@ from tap_notion.streams.comments import Comments
 from tap_notion.streams.bot_user import BotUser
 from tap_notion.streams.file_upload import FileUpload
 from tap_notion.streams.user import User
-from tap_notion.streams.file_upload_list import FileUploadList
 from tap_notion.streams.file_uploads_list import FileUploadsList
 from tap_notion.streams.block_children import BlockChildren
-from tap_notion.streams.page_property import PagesProperty
+from tap_notion.streams.page_property import PageProperty
 
 STREAMS = {
     "pages": Pages,
@@ -20,9 +19,8 @@ STREAMS = {
     "bot_user": BotUser,
     "file_upload": FileUpload,
     "user": User,
-    "file_upload_list": FileUploadList,
     "file_uploads_list": FileUploadsList,
     "block_children": BlockChildren,
-    "pages_property": PagesProperty,
+    "page_property": PageProperty,
 
 }
