a561f4394eb8fff218dd444d5484c0d27143f67b
Rahul Gogawale <rahul.gogawale@globallogic.com>
2025-08-13 11:41:39 +0530

    Added changes as per the comments.

--- tap_notion/streams/__init__.py
+++ tap_notion/streams/__init__.py
@@ -5,8 +5,6 @@ from tap_notion.streams.blocks import Blocks
 from tap_notion.streams.comments import Comments
 from tap_notion.streams.bot_user import BotUser
 from tap_notion.streams.file_upload import FileUpload
-from tap_notion.streams.user import User
-from tap_notion.streams.file_uploads_list import FileUploadsList
 from tap_notion.streams.block_children import BlockChildren
 from tap_notion.streams.page_property import PageProperty
 
@@ -18,8 +16,6 @@ STREAMS = {
     "comments": Comments,
     "bot_user": BotUser,
     "file_upload": FileUpload,
-    "user": User,
-    "file_uploads_list": FileUploadsList,
     "block_children": BlockChildren,
     "page_property": PageProperty,
 
--- tap_notion/streams/file_upload.py
+++ tap_notion/streams/file_upload.py
@@ -7,6 +7,9 @@ LOGGER = get_logger()
 class FileUpload(FullTableStream):
     tap_stream_id = "file_upload"
     key_properties = ["id"]
-    replication_keys = []
-    replication_method = "FULL_TABLE"
-    path = "/v1/files/{file_id}"
+    replication_keys = ["last_edited_time"]
+    replication_method = "INCREMENTAL"
+    path = "files"
+
+    def parse_response(self, response):
+        yield from response.json().get("results", [])
